6f7da1fc5602257102cd250a2f43a5b95bd3e264
Lalit Rana <lalit.rana@nagarro.com>
2021-08-13 18:00:04 +0530

    Ex1 project created

---
+++ ConsoleAppEx1/ConsoleAppEx1/LinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEx1
+{
+    class LinkedList
+    {
+        /*
+         * [ ] insert : insert after last node
+         * [ ] insert at position
+         * [ ] delete : ???
+         * [ ] delete at position 
+         * [ ] center
+         * [ ] reverse
+         * [ ] size
+         * [ ] iterator 
+         * [ ] traverse/print
+         */
+    }
+}
---
+++ ConsoleAppEx1/ConsoleAppEx1/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEx1
+{
+    class Node
+    {
+        /*
+         * [ ] value
+         * [ ] head
+         * [ ] reverse head
+         * 
+         * Constructor
+         * 
+         * 
+         * 
+         */
+    }
+}
